0d30593fb9d57c1216f298e633e8997e86317b2d
Satish980 <isatishkumar4008@gmail.com>
2022-01-27 14:49:53 +0530

    Consoles removed

--- src/App.js
+++ src/App.js
@@ -1,45 +1,38 @@
-
-import './App.css';
-import Header from './components/Header'
-import {useState, useEffect} from 'react'
-import axios from 'axios';
-import PunkList from './components/PunkList';
-import Main from './components/Main';
+import "./App.css";
+import Header from "./components/Header";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import PunkList from "./components/PunkList";
+import Main from "./components/Main";
 
 function App() {
-  const [punkListData, setPunkListData] = useState([])
-  const [selectedPunk, setSelectedPunk] = useState(0)
+  const [punkListData, setPunkListData] = useState([]);
+  const [selectedPunk, setSelectedPunk] = useState(0);
   useEffect(() => {
     const getMyNfts = async () => {
-       const openseaData = await axios.get(
-         'https://testnets-api.opensea.io/assets?order_direction=asc&asset_contract_address=0x855Ee71F38512dc100E00DAa3026f1e5B23197d1',
-         
-       )
+      const openseaData = await axios.get(
+        "https://testnets-api.opensea.io/assets?order_direction=asc&asset_contract_address=0x855Ee71F38512dc100E00DAa3026f1e5B23197d1"
+      );
+      setPunkListData(openseaData.data.assets);
+    };
 
-       console.log(openseaData.data.assets);
-       setPunkListData(openseaData.data.assets)
-    }
-
-    return getMyNfts()
-  }, [])
-
+    return getMyNfts();
+  }, []);
 
   return (
-    <div className='app'>
-    <Header/>
-    {
-      punkListData.length > 0 && (
+    <div className="app">
+      <Header />
+      {punkListData.length > 0 && (
         <>
-          <Main punkListData={punkListData} selectedPunk={selectedPunk}/>
-          <PunkList 
-           punkListData={punkListData} 
-           setSelectedPunk={setSelectedPunk}
+          <Main punkListData={punkListData} selectedPunk={selectedPunk} />
+          <PunkList
+            punkListData={punkListData}
+            setSelectedPunk={setSelectedPunk}
           />
         </>
-      )
-    }
-  </div>
-  )
+      )}
+    </div>
+  );
 }
 
 export default App;
