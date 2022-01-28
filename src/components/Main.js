1d1335bdd1ffabe9bd90ae3f8f8b431ae272aa2a
Satish980 <isatishkumar4008@gmail.com>
2022-01-28 16:54:37 +0530

    "CSS Styles Changed"

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,58 +1,53 @@
-import React,{useState, useEffect} from 'react';
-import instagramLogo from '../assets/owner/instagram.png';
-import twitterLogo from './../assets/owner/twitter.png';
-import moreIcon from '../assets/owner/more.png';
-import './Main.css';
+import React, { useState, useEffect } from "react";
+import instagramLogo from "../assets/owner/instagram.png";
+import twitterLogo from "./../assets/owner/twitter.png";
+import moreIcon from "../assets/owner/more.png";
+import "./Main.scss";
 
-const Main = ({ selectedPunk, punkListData}) => {
-    const [activePunk, setActivePunk] = useState(punkListData[0])
-    console.log('🔥');
-    useEffect(() => {
-        setActivePunk(punkListData[selectedPunk])
-    }, [punkListData, selectedPunk])
+const Main = ({ selectedPunk, punkListData }) => {
+  const [activePunk, setActivePunk] = useState(punkListData[0]);
+  console.log("🔥");
+  useEffect(() => {
+    setActivePunk(punkListData[selectedPunk]);
+  }, [punkListData, selectedPunk]);
   return (
-    <div className='main'>
-        <div className='mainContent'>
-            <div className='punkHighlight'>
-                <div className='punkContainer'>
-                    <img
-                    className='selectedPunk'
-                    src={activePunk.image_url}
-                    alt = 'selectedPunk'
-                    />
-                </div>
-            </div>
-
-            <div className='punkDetails' style={{color:'#fff'}}>
-                <div className='title'>{activePunk.name}</div>
-                <span className='itemNumber'>.#{activePunk.token_id}</span>
-            </div>
-            <div className='owner'>
-                <div className='ownerImageContainer'>
-                    <img
-                      src = {
-                          activePunk.image_url
-                      }
-                      alt = 'ownerImage'
-                    />
-                </div>
-                <div className='ownerDetails'>
-                    <div className='ownerNameAndHandle'>
-                      <div className=''>0x855Ee71F38512dc100E00DAa3026f1e5B23197d1</div>
-                      <div className='ownerHandle'>@satish980</div>
-                    </div>
-                    <div className='ownerLink'>
-                        <img src={instagramLogo} alt=''/>
-                    </div>
-                    <div className='ownerLink'>
-                        <img src={twitterLogo} alt=''/>
-                    </div>
-                    <div className='ownerLink'>
-                        <img src={moreIcon} alt=''/>
-                    </div>
-                </div>
-            </div>
+    <div className="main">
+      <div className="mainContent">
+        <div className="punkHighlight">
+          <div className="punkContainer">
+            <img
+              className="selectedPunk"
+              src={activePunk.image_url}
+              alt="selectedPunk"
+            />
+          </div>
         </div>
+
+        <div className="punkDetails" style={{ color: "#fff" }}>
+          <div className="title">{activePunk.name}</div>
+          <span className="itemNumber">.#{activePunk.token_id}</span>
+        </div>
+        <div className="owner">
+          <div className="ownerImageContainer">
+            <img src={activePunk.image_url} alt="ownerImage" />
+          </div>
+          <div className="ownerDetails">
+            <div className="ownerNameAndHandle">
+              <div className="">0x855Ee71F38512dc100E00DAa3026f1e5B23197d1</div>
+              <div className="ownerHandle">@satish980</div>
+            </div>
+            <div className="ownerLink">
+              <img src={instagramLogo} alt="" />
+            </div>
+            <div className="ownerLink">
+              <img src={twitterLogo} alt="" />
+            </div>
+            <div className="ownerLink">
+              <img src={moreIcon} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
